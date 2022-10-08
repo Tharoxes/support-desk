138ee695a02afc6eec25b502a164c9a43a4c9189
Thanh <thc.huynh@protonmail.com>
2022-10-08 12:08:15 +0200

    Header, Login, Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-     My App
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+        <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes> 
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
